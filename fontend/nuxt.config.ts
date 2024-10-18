18cb7edb389e0e6d02d9b339418da49c294f9607
eris-vn <sonn88918@gmail.com>
2024-10-19 02:51:34 +0700

    Update nuxt.config.ts

--- fontend/nuxt.config.ts
+++ fontend/nuxt.config.ts
@@ -3,17 +3,32 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@primevue/nuxt-module", "@nuxt/icon", "@nuxt/fonts", "nuxt-vue3-google-signin"],
+  modules: [
+    "@primevue/nuxt-module",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "nuxt-vue3-google-signin",
+  ],
   primevue: {
     importTheme: { from: "@/themes/main.js" },
   },
   googleSignIn: {
-    clientId: '81902586117-qin78jipkm7eu8e41tqtqtiv5vu0tdv0.apps.googleusercontent.com',
+    clientId:
+      "81902586117-qin78jipkm7eu8e41tqtqtiv5vu0tdv0.apps.googleusercontent.com",
   },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
     },
   },
 });
